9b75f47cef6634f6d06e03fc4d429ffd2b8c4871
ksenia.glotova <ksenia.a.glotova@gmail.com>
2026-08-19 19:47:06 +0200

    test page updated with pictures

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,14 +1,41 @@
 import './Test.css'
 
+export function Test() {
 const images: string[] = [
   "/moped-test.jpg",
   "motorrad-bild.jpg",
   "/motorrad-bild2.jpg",
-  "/homepage-background.jpg",
-  
+  "/homepage-background.jpg",  
 ];
-export function Test() {
-  const repeatedImages: string[] = [...images, ...images];
+
+const repeatedImages: string[] = [...images, ...images];
+
+const images2: string[] = [
+  "/food/bowl.jpg",
+  "/food/pizza1.jpg",
+];
+
+const repeatedImages2: string[] = [...images2, ...images2];
+
+const images3: string[] = [
+  "/oldpics/estonia-beach.jpg",
+  "/oldpics/gymnastics.jpg",
+  "/oldpics/towel.JPG",
+];
+
+const repeatedImages3: string[] = [...images3, ...images3];
+
+const images4: string[] = [
+  "/places/cassis-sea.jpg",
+  "/places/france-cassis.jpg",
+  "/places/moskau-city.jpg",
+  "/places/moskau.jpg",
+  "/places/sankt-petersburg.jpg",
+];
+
+const repeatedImages4: string[] = [...images4, ...images4];
+
+
 
   return (
     <div className="test-layout">
@@ -34,11 +61,48 @@ export function Test() {
         </p>
       </div>
 
-      <div className="test-text">
-        <p>hallo</p>
+      <div className="image-rail" aria-label="Scrolling image gallery">
+        <div className="image-track">
+          {repeatedImages2.map((image, index) =>
+            (
+              <img
+                key={index} 
+                src={image} 
+                alt={"Index: "+index} 
+               />
+            )
+          )}
+        </div>
       </div>
 
-      
+      <div className="image-rail" aria-label="Scrolling image gallery">
+        <div className="image-track">
+          {repeatedImages3.map((image, index) =>
+            (
+              <img
+                key={index} 
+                src={image} 
+                alt={"Index: "+index} 
+               />
+            )
+          )}
+        </div>
+      </div>
+
+      <div className="image-rail" aria-label="Scrolling image gallery">
+        <div className="image-track">
+          {repeatedImages4.map((image, index) =>
+            (
+              <img
+                key={index} 
+                src={image} 
+                alt={"Index: "+index} 
+               />
+            )
+          )}
+        </div>
+      </div>
+
     </div>
     
   )
